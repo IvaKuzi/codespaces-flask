75e3a43995e7f7aea4a7be6cab6800d2283b2aa2
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-09-17 00:58:11 +0200

    reset app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("hello.html",name="world")
-
-@app.route("/<title>")
-def index(title):
-    return render_template("hello.html",name=title)
+def hello_world():
+    return render_template("index.html", title="Hello")
 
 

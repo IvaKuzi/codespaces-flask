9d9e49bc259ee85c542b1dc3dac0f991f91d7a1e
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-09-23 18:48:46 +0000

    Hello ***** !

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("hello.html",name="world")
+    return render_template("hello.html",name="home")
 
 @app.route("/<title>")
 def index(title):

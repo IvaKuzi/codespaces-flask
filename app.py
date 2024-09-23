d55389a6af1eb4ca1126b0b37bf2016153f001f5
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-09-23 18:24:01 +0000

    U2 17.09.2024

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    #return render_template("index.html", title="Hello")
+    print("/")
+    return render_template("hello_world.html")
 
+@app.route("/home")
+def home():
+    print("/home")
+    return redirect( url_for("hello_world") )
 
+@app.route("/about")
+def about():
+    return "About Us"
+
+@app.route("/preise")
+def preise():
+    return "<h1>Unsere Tarife</h1>"
+
+#impressum
+#preise
+#kontakte
+#teilen
+#galerie

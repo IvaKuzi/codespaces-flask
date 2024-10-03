1e5ba9f986541af8ef81d15fb7796de3de2520c7
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-10-03 16:00:16 +0000

    1st commit

--- app.py
+++ app.py
@@ -6,28 +6,4 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/home")
-def home():
-    return redirect( url_for("index") )
-
-@app.route("/ueber-uns")
-def about():
-    return render_template("about.html")
-
-@app.route("/preise")
-def preise():
-    return render_template("prices.html")
-
-@app.route("/kontakt", methods=['POST', 'GET'])
-def contact():
-    if request.method == 'POST':
-        print(request.form)
-        print(f"Name: {request.form['name']}")
-        print(f"E-Mail: {request.form['email']}")
-    return render_template("contact.html")
-
-# generic page
-@app.route("/<address>")
-def generic(address):
-    return render_template("generic.html", title=address)
     

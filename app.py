29963a05ed66a112a75b77722f1784579ae4f420
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-10-06 19:44:06 +0000

    working application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask_socketio import SocketIO
-import time
+import time, json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -14,17 +14,38 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+def getHistory(file_name):
+    data = []
+    # Open the existing file and read its content
+    try:
+        with open(file_name, 'r') as json_file:
+            # Check if file is empty
+            try:
+                data = json.load(json_file)  # Load existing data
+            except json.JSONDecodeError:
+                data = []  # If file is empty or invalid, start with an empty list
+    except FileNotFoundError:
+        data = []  # If file does not exist, start with an empty list
+    
+    if not isinstance(data, list):
+        data = [] # If file is empty or invalid, start with an empty list
+    
+    return data
+
 # Track connection
 @socketio.on('connect')
 def handle_connect():
     print(f'Client connected: {request.sid}')
-
+    file_name = 'messages.json'
+    data = getHistory(file_name)
+    socketio.emit('all-messages', data)
+    
 # Track disconnection
 @socketio.on('disconnect')
 def handle_disconnect():
     print(f'Client disconnected: {request.sid}')
-    # Log or take action when a client disconnects
 
+# Track message submission
 @socketio.on('message-submit')
 def handle_message(payload):
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
@@ -32,10 +53,13 @@ def handle_message(payload):
     content = payload['content']
     payload['timestamp'] = timestamp
     print(f'({timestamp}) Message from {user} received: {content}')
-    socketio.emit('new-message', payload)
-    with open('messages.txt', 'a') as file:
-        file.write(f"{payload},\n")
-    
 
+    file_name = 'messages.json'
+    data = getHistory(file_name)
+    data.append(payload)
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)  # Write updated data back to the file
+    socketio.emit('all-messages', data)
+    
 if __name__ == '__main__':
     socketio.run(app, debug=True)

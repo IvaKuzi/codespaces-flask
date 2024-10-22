229b9cf60b436b97e3c37208fe96b07ed650abe2
IvaKuzi <142713543+IvaKuzi@users.noreply.github.com>
2024-10-22 17:32:49 +0000

    added chat.html, flask-socketio lib to requirements.txt, sockeio Object to app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask import flash
+from flask_socketio import SocketIO
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ssdasdmn,a.fgj.arshklgwewet'
+socketio = SocketIO(app)
 
 def custom_render(url):
     if 'username' in session:
         username = session['username']
     else:
-        username = 'Nicht eingelogt'
+        username = ''
     return render_template(url, user=username)
 
 @app.route("/")
@@ -29,12 +33,15 @@ def preise():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    username = 'Nicht eingelogt'
+    username = ''
     if request.method == 'POST':
         username = request.form['username']
         print(username)
         session['username'] = username
-        flash(f'{username} eingelogt')
+        if username == "":
+            flash(f'username incorrect')
+        else:
+            flash(f'{username} eingelogt')
     return custom_render("login.html")
 
 @app.route("/logout")
@@ -58,4 +65,23 @@ def contact():
 @app.route("/<address>")
 def generic(address):
     return render_template("generic.html", title=address)
+
+@app.route("/chat")
+def chat():
+    return custom_render("chat.html")
     
+# Track connection
+@socketio.on('connect')
+def handle_connect():
+    sid = request.sid # session id
+    ip = request.remote_addr # IP
+    username = session['username']
+    print(f"Client connected: {sid} (IP: {ip}) (username: {username})")
+
+# Track disconnection
+@socketio.on('disconnect')
+def handle_disconnect():
+    sid = request.sid # session id
+    ip = request.remote_addr # IP
+    username = session['username']
+    print(f'Client disconnected: {sid}  (IP: {ip}) (username: {username})')
